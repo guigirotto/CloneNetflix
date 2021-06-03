4f41b0d31df93f0c325a74b553d6fcfa5494b9be
Guilherme Girotto <guilherme.girotto@raccoon.ag>
2021-06-02 21:37:47 -0300

    começando as listas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import './App.css';
+
+
 
 export default () =>{
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () =>{
+      // Pegando a lista de filmes
+
+      let list =await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+
+
   return(
-    <div>
-      Eai tiu
+    <div className="page">
+        <section className="lists">
+          {movieList.map((item, key)=>(
+            <div>
+              <MovieRow key={key} title={item.title} items={item.items}/>
+            </div>
+          ))}
+        </section>
     </div>
   )
 };
