cce4c862fbe5d837c244f546e16c17e4f80526da
Guilherme Girotto <guigirotto99@hotmail.com>
2021-06-03 21:27:32 -0300

    imagem, titulo e conteudo do filme em destaque

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, {useEffect, useState} from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css';
-
+import FeaturedMovie from './components/FeaturedMovie';
 
 
 export default () =>{
 
   const [movieList, setMovieList] = useState([]);
+  const [featueredData, setFeatureueredData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () =>{
@@ -15,21 +16,37 @@ export default () =>{
 
       let list =await Tmdb.getHomeList();
       setMovieList(list);
+
+          // pegando filme em destaque
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeatureueredData(chosenInfo);
     }
 
+
+    
     loadAll();
   }, []);
 
 
   return(
     <div className="page">
-        <section className="lists">
+
+      {featueredData && 
+        <FeaturedMovie item={featueredData}/>
+      }
+      
+
+      <section className="lists">
           {movieList.map((item, key)=>(
             <div>
               <MovieRow key={key} title={item.title} items={item.items}/>
             </div>
           ))}
-        </section>
+      </section>
     </div>
   )
 };
