42920a2b8c6288c83e0503a34605c9bca901cfa4
Guilherme Girotto <guigirotto99@hotmail.com>
2021-06-27 19:05:56 -0300

    header, conteudo principal finalizado

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
 import './App.css';
 import FeaturedMovie from './components/FeaturedMovie';
-
+import Header from './components/Header';
 
 export default () =>{
 
   const [movieList, setMovieList] = useState([]);
   const [featueredData, setFeatureueredData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
     const loadAll = async () =>{
@@ -31,10 +32,26 @@ export default () =>{
     loadAll();
   }, []);
 
+  useEffect(() =>{
+    const scrollListener = () =>{
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+    return () =>{
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
 
   return(
     <div className="page">
 
+      <Header black={blackHeader}/>
+      
       {featueredData && 
         <FeaturedMovie item={featueredData}/>
       }
